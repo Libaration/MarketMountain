460c559a2d3b79a3537310b57f6824f466ef28fd
Cristian Deleon <libaration@gmail.com>
2021-10-18 02:55:28 -0400

    adds banners

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,7 +1,9 @@
 import React, { ReactElement } from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import logo from '../images/logo.png';
+import banner1 from '../images/banner1.png';
+import banner2 from '../images/banner2.png';
+import banner3 from '../images/banner3.png';
 interface Props {}
 
 function Slider({}: Props): ReactElement {
@@ -13,12 +15,17 @@ function Slider({}: Props): ReactElement {
         autoPlay={true}
         showThumbs={false}
         showStatus={false}
+        dynamicHeight={false}
       >
         <div>
-          <img src={logo} />
+          <img src={banner2} alt="banner" />
         </div>
-        <div>sdsdsdsd</div>
-        <div>sddsdsds</div>
+        <div>
+          <img src={banner1} alt="banner" />
+        </div>
+        <div>
+          <img src={banner3} alt="banner" />
+        </div>
       </Carousel>
     </div>
   );
